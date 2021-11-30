66391a59135223733ed20c5f082d220812da51e6
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-11-29 21:29:37 -0600

    First challenge

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
-    const categories = ['Liverpool', 'The Beatles', 'Kirby'];
+    const [categories, setCategories] = useState(['Liverpool', 'The Beatles', 'Kirby'])
+    
+    const handleAdd = () => {
+        const newArr = [...categories, 'Tacos'];
+        setCategories(newArr)
+    }
     
     return (
         <>
         <h2>GifExpertApp</h2>
         <hr/>
+        
+        <button onClick={handleAdd}>add</button>
+        
         <ol>
             {
                 categories.map(
                     (category) => {
                         return <li key={category}>
-                                {category} </li>
+                            {category} 
+                        </li>
                     }
                 )
             }
