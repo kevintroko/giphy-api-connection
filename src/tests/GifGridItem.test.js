84fa7868ee0d2c79fdea07d3425efc6a5dc800c4
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-16 21:43:34 -0600

    Update readme

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -14,5 +14,23 @@ describe('Fist app', () => {
         
         test('should match snapshot', () => {  
             expect(wrapper).toMatchSnapshot();
-        });    
+        }); 
+        
+        test('should have a p with the title', () => {  
+            const p = wrapper.find('p');
+            expect(p.text()).toBe(title);
+        }); 
+        
+        test('should have and image equal to the url and alt of props', () => {  
+            const img = wrapper.find('img');
+            expect(img.props().src).toBe(url);
+            expect(img.prop('alt')).toBe(title);
+        });
+        
+        test('should class name', () => {  
+            const div = wrapper.find('div');
+            const className = div.prop('className');
+            expect(className.includes('animate__fadeIn')).toBe(true);
+            
+        });
 });
